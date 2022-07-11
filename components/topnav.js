0a9f3ded67fa9b3fd4f08b120fa71b6a446507ff
Bazil Zainal <bazilzainal@gmail.com>
2022-07-11 13:55:28 +0800

    Add clickHandlers to topnav, change favicon

--- components/topnav.js
+++ components/topnav.js
@@ -2,39 +2,45 @@ import Link from "next/link";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Image from "next/image";
 import logo from "../public/images/logo.png";
-import { Button } from "react-bootstrap";
 import styles from "./topnav.module.css";
+import { useState } from 'react';
 
 export default function TopNav() {
+
+    const [show, setShow] = useState(false);
+
+    function handleClick() {
+        setShow(false);
+    }
+
     return (
         <>
-            <Navbar className={styles.topnav} expand="md" style={{ justifyContent: `space-around` }}>
+            <Navbar expanded={show} className={styles.topnav} expand="md" style={{ justifyContent: `space-around` }}>
                 <Container className="mx-auto">
                     <Link href="/" passHref>
                         <Navbar.Brand>
-                            <Image src={logo} width={70} height={70}></Image>
+                            <Image src={logo} width={70} height={70} onClick={handleClick}></Image>
                         </Navbar.Brand>
                     </Link>
                     <Link href="/">
-                        <a className={styles.button}>Buy Tickets</a>
+                        <a className={styles.button} onClick={handleClick}>Buy Tickets</a>
                     </Link>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Toggle className={styles.toggler} onClick={() => setShow(show ? false : "expanded")} />
+                    <Navbar.Collapse>
                         <Nav className={styles.navsection}>
-                            <Link href="/" passHref>
-                                <Nav.Link>Home</Nav.Link>
+                            <Link href="/" passHref >
+                                <Nav.Link onClick={handleClick}>Home</Nav.Link>
                             </Link>
                             <Link href="/tickets" passHref>
-                                <Nav.Link>Tickets</Nav.Link>
+                                <Nav.Link onClick={handleClick}>Tickets</Nav.Link>
                             </Link>
                             <Link href="/about" passHref>
-                                <Nav.Link>About</Nav.Link>
+                                <Nav.Link onClick={handleClick}>About</Nav.Link>
                             </Link>
                             <Link href="/contact" passHref>
-                                <Nav.Link>Contact</Nav.Link>
+                                <Nav.Link onClick={handleClick}>Contact</Nav.Link>
                             </Link>
                             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
