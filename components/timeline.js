81bcb4b7265a73a28ba596fde1b42f6c7f0d2448
Bazil Zainal <bazilzainal@gmail.com>
2022-07-26 20:02:12 +0800

    Add DatoCMS for Programme, add cover image for each programme

--- components/timeline.js
+++ components/timeline.js
@@ -1,5 +1,7 @@
 import styles from "./timeline.module.scss";
-import Date from "./date";
+import { StructuredText } from "react-datocms";
+import { Image } from "react-datocms";
+import { isSameDay, format } from "date-fns";
 
 export default function Timeline({ allEventsData }) {
     return (
@@ -7,81 +9,89 @@ export default function Timeline({ allEventsData }) {
             <h1>Programme</h1>
             <section>
                 <div className={styles.gridWrapper}>
+
+                    {/* First day */}
                     <h2>1 September 2022</h2>
                     {allEventsData.map((event) => {
                         return (
-                            event.date == "2022-09-01" && (
-                                <article key={event.id} className={styles.event}>
-                                    <div className={styles.event__content}>
+                            isSameDay(new Date(event.startDate), new Date(2022, 8, 1)) && (
+                                <article key={event.slug} className={styles.event}>
+                                    <div className={styles.eventImage}>
+                                        <Image data={event.coverImage.responsiveImage}></Image>
+                                    </div>
+                                    <div className={styles.eventContent}>
+                                        <h4>{format(new Date(event.startDate), "HH:mmaa")} - {format(new Date(event.endDate), "HH:mmaa")}</h4>
                                         <h3>{event.title}</h3>
-                                        <h4>
-                                            {event.startTime} - {event.endTime}
-                                        </h4>
-                                        <p>{event.description}</p>
+                                        <p>{event.excerpt}</p>
                                     </div>
                                 </article>
                             )
                         );
                     })}
                 </div>
-                
                 <div className={styles.gridWrapper}>
+
+                    {/* Second day */}
                     <h2>2 September 2022</h2>
                     {allEventsData.map((event) => {
                         return (
-                            event.date == "2022-09-02" && (
-                                <article key={event.id} className={styles.event}>
-                                    <div className={styles.event__content}>
+                            isSameDay(new Date(event.startDate), new Date(2022, 8, 2)) && (
+                                <article key={event.slug} className={styles.event}>
+                                    <div className={styles.eventImage}>
+                                        <Image data={event.coverImage.responsiveImage}></Image>
+                                    </div>
+                                    <div className={styles.eventContent}>
+                                        <h4>{format(new Date(event.startDate), "hh:mmaa")} - {format(new Date(event.endDate), "HH:mmaa")}</h4>
                                         <h3>{event.title}</h3>
-                                        <h4>
-                                            {event.startTime} - {event.endTime}
-                                        </h4>
-                                        <p>{event.description}</p>
+                                        <p>{event.excerpt}</p>
                                     </div>
                                 </article>
                             )
                         );
                     })}
                 </div>
-                
                 <div className={styles.gridWrapper}>
+
+                    {/* Second day */}
                     <h2>3 September 2022</h2>
                     {allEventsData.map((event) => {
                         return (
-                            event.date == "2022-09-03" && (
-                                <article key={event.id} className={styles.event}>
-                                    <div className={styles.event__content}>
+                            isSameDay(new Date(event.startDate), new Date(2022, 8, 3)) && (
+                                <article key={event.slug} className={styles.event}>
+                                    <div className={styles.eventImage}>
+                                        <Image data={event.coverImage.responsiveImage}></Image>
+                                    </div>
+                                    <div className={styles.eventContent}>
+                                        <h4>{format(new Date(event.startDate), "hh:mmaa")} - {format(new Date(event.endDate), "HH:mmaa")}</h4>
                                         <h3>{event.title}</h3>
-                                        <h4>
-                                            {event.startTime} - {event.endTime}
-                                        </h4>
-                                        <p>{event.description}</p>
+                                        <p>{event.excerpt}</p>
                                     </div>
                                 </article>
                             )
                         );
                     })}
                 </div>
-                
                 <div className={styles.gridWrapper}>
+
+                    {/* Second day */}
                     <h2>4 September 2022</h2>
                     {allEventsData.map((event) => {
                         return (
-                            event.date == "2022-09-04" && (
-                                <article key={event.id} className={styles.event}>
-                                    <div className={styles.event__content}>
+                            isSameDay(new Date(event.startDate), new Date(2022, 8, 4)) && (
+                                <article key={event.slug} className={styles.event}>
+                                    <div className={styles.eventImage}>
+                                        <Image data={event.coverImage.responsiveImage}></Image>
+                                    </div>
+                                    <div className={styles.eventContent}>
+                                        <h4>{format(new Date(event.startDate), "hh:mmaa")} - {format(new Date(event.endDate), "HH:mmaa")}</h4>
                                         <h3>{event.title}</h3>
-                                        <h4>
-                                            {event.startTime} - {event.endTime}
-                                        </h4>
-                                        <p>{event.description}</p>
+                                        <p>{event.excerpt}</p>
                                     </div>
                                 </article>
                             )
                         );
                     })}
                 </div>
-                
             </section>
         </div>
     );
