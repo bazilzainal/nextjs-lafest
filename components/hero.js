ac11a8cc67b05b9e54ab6adfc632b4123a655cb4
Bazil Zainal <bazilzainal@gmail.com>
2022-08-12 09:38:27 +0800

    Change hero image

--- components/hero.js
+++ components/hero.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import heroPic from "../public/images/birds.png";
+import heroPic from "../public/images/poster.png";
 import styles from "./hero.module.css";
 
 export default function Hero() {
     return (
         <>
             <div className={styles.imgDiv}>
-                <div className={styles.herotext}>
+                {/* <div className={styles.herotext}>
                     <h1>LANGKAWI WRITERS' FESTIVAL</h1>
                     <span>1 - 4 September 2022</span>
-                </div>
+                </div> */}
                 <Image
                     className={styles.heroImg}
                     src={heroPic}
